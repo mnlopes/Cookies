cf871ec9e17ad753176d998585ba26764e012ca8
mnlopes <marcelo.fachas.lopes@gmail.com>
2025-11-19 12:57:05 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+    plugins:[react()],
+    base:"/Cookies",
     const env = loadEnv(mode, '.', '');
     return {
       server: {
